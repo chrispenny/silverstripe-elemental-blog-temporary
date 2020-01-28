286a7e51b772473bf04a0dd6d6d85980c69d034e
cpenny <cpenny@silverstripe.com>
2020-01-28 13:26:28 +1300

    Initial implementation of Overview Block and docs

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
 # silverstripe-elemental-blog
+
+## Purpose
+
+The purpose of this module is to provide you with an Elemental Block/Element that can be used to replace the default
+Blog template behaviour (EG: Use a Block to display a paginated list of Blog Posts, with support for pagination,
+categorisation, and tags).
+
+As there are many ways that you could be using Elemental, this module does **not** make assumptions about how you want
+it to be applied. Examples are provided below.
+
+## Installation
+
+```
+composer require chrispenny/silverstripe-elemental-blog
+```
+
+The Elemental module suggests that we override the `Layout` template, and use `$ElementalArea` to render the elements to
+the page (in place of `$Content`). The following assumes that this is how you will be implementing your Blocks.
+
+The Blog module itself uses the standard `Page.ss` template, but also provides it's own `Layout` template
+(`SilverStripe\Blog\Model\Layout\Blog.ss`). So, out of the box, even if `Elemental` is available to your `Blog` page, no
+Elements are going to be output in your template (as Silverstripe will first check for, and find, a `Layout\Blog`
+template).
+
+For us, using the Blog module, that means that we'll likely want to override the default Blog `Layout` template. The
+idea being that we now want to render all aspects of the Blog through Blocks, rather than just through the `Layout`
+template.
+
+To do that, in the templates directory of your active theme (EG: `/themes/simple/templates/...`), add a new Layout
+template matching the namespace of the Blog module's `Layout` template (`SilverStripe\Blog\Model\Layout\Blog.ss`).
+
+So, your directories should look something like:
+
+```
+themes /
+  simple /
+    templates /
+      SilverStripe /
+        Blog /
+          Model /
+            Layout/
+              Blog.ss
+```
+
+In your (new) `Blog.ss` file, you can now output `$ElementalArea` where you would like it.
+
+EG `Blog.ss`:
+
+```
+<div class="content-container">
+    $ElementalArea
+</div>
+```
+
+## Usage
+
+### Blog Overview Block
+
+This is (likely) the main Block that you will be using. It will output a bunch of what was originally being output by
+the Blog module's `Layout` template.
+
+Including:
+
+- Title (including Category/Archive/etc titles) (if `$ShowTitle` is ticked in the CMS for the Block)
+- Blog Posts that were created under this particular Blog page (or simply **all** Blog Posts if this Block is not being
+used on a Blog page)
+- Pagination (if `$ShowPagination` is ticked in the CMS for the Block, and if the `Controller` for the page is able to
+return a `PaginatedList`)
+- Widgets (if `$ShowWidgets` is ticked in the CMS for the Block and if the Page is able to return a `WidgetArea`)
+
+But **not** including:
+
+- `$Content` (it is assumed, since you're building pages with Blocks, that your `$Content` will be coming from another
+block)
+
+You will likely want to override the very basic default template that I have provided, you can do so by overriding the
+template found with the namespace `ChrisPenny\ElementalBlog\Model\BlogOverviewBlock.ss`.
+
+**Please consider:** While the Overview Block does support you using it on other page types, it is primarily designed to
+be used on Blog page types. This is because it is `Blog` and `BlogController` that provide the relevant info to this
+Block. If you use this Block on other page types, then this Block's default behaviour is simply to return all Blog Posts
+currently in the DB.
+
+Please consider whether you want this Block to be available to other page types, and if you don't, you might want to
+add this Block as a `disallowed` Element on your base Block page. EG:
+
+```yaml
+Page:
+  disallowed_elements:
+    - ChrisPenny\ElementalBlog\Model\BlogOverviewBlock
+```
+
+## Blog Pagination Block
+
+Coming soon
+
+## Blog Widgets Block
+
+Coming soon
---
+++ src/Model/BlogOverviewBlock.php
@@ -0,0 +1,356 @@
+<?php
+
+namespace ChrisPenny\ElementalBlog\Model;
+
+use DNADesign\Elemental\Models\BaseElement;
+use SilverStripe\Blog\Model\Blog;
+use SilverStripe\Blog\Model\BlogController;
+use SilverStripe\Blog\Model\BlogPost;
+use SilverStripe\Control\Controller;
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\LiteralField;
+use SilverStripe\ORM\DataList;
+use SilverStripe\ORM\PaginatedList;
+use SilverStripe\ORM\ValidationException;
+use SilverStripe\Widgets\Extensions\WidgetPageExtension;
+use SilverStripe\Widgets\Model\WidgetArea;
+
+/**
+ * Class BlogOverviewBlock
+ *
+ * @package ChrisPenny\ElementalBlog\Model
+ * @property int $ShowPagination
+ * @property int $ShowWidgets
+ */
+class BlogOverviewBlock extends BaseElement
+{
+    /**
+     * @var array
+     */
+    private static $db = [
+        'ShowPagination' => 'Boolean',
+        'ShowWidgets' => 'Boolean',
+    ];
+
+    /**
+     * @var string
+     */
+    private static $icon = 'font-icon-p-articles';
+
+    /**
+     * @var string
+     */
+    private static $table_name = 'BlogOverviewBlock';
+
+    /**
+     * @var string
+     */
+    private static $singular_name = 'Blog overview block';
+
+    /**
+     * @var string
+     */
+    private static $plural_name = 'Blog overview blocks';
+
+    /**
+     * @var string
+     */
+    private static $description = 'Block displaying Blog Posts with pagination';
+
+    /**
+     * We use this default_title for the Block name in the CMS. Feel free to update it via config
+     *
+     * @var string
+     */
+    private static $default_title = 'Blog Overview';
+
+    /**
+     * OotB there is really no reason for a content author to enter this Block to make any edits, so, by default, we'll
+     * just set a generic default Title. You can disable this via config
+     *
+     * @var bool
+     */
+    private static $set_default_title = true;
+
+    /**
+     * By default, we show the "Show Pagination" field in the CMS (since it is part of the default supported features).
+     * You may, however, prefer that content authors display pagination for the Blog using the specific
+     * `PaginationBlock`, and if that's the case, you'll likely want to set this value to `0` via config, as it will
+     * no longer be relevant for your content authors
+     *
+     * @var int
+     */
+    private static $show_pagination_field = 1;
+
+    /**
+     * Default value for ShowPagination. If set, this block will also output the pagination for your Blog. You can
+     * update this value via config
+     *
+     * @var int
+     */
+    private static $pagination_field_default = 1;
+
+    /**
+     * Since the Widgets module is an addon for the Blog module (and not out of the box), we've hidden the CMS field by
+     * default (using this config). You can update this via config. If set to `1`, we will display the CMS field for
+     * your users to use
+     *
+     * @var int
+     */
+    private static $show_widgets_field = 0;
+
+    /**
+     * Since the Widgets module is an addon for the Blog module (and not out of the box), we've set the default for this
+     * field to be `0`. You can update this via config
+     *
+     * @var int
+     */
+    private static $widgets_field_default = 0;
+
+    /**
+     * This can be updated via config if (for whatever reason) you do not wish to show this message field in the CMS
+     *
+     * @var int
+     */
+    private static $show_info_message_field = 1;
+
+    /**
+     * Cached value for BlogPosts from the Blog page
+     *
+     * @var DataList|BlogPost[]|null
+     */
+    private $blogPosts;
+
+    /**
+     * Cached value for the PaginatedList from BlogController
+     *
+     * @var PaginatedList|null
+     */
+    private $paginatedList;
+
+    /**
+     * Cached value for WidgetArea from Blog page
+     *
+     * @var WidgetArea|null
+     */
+    private $widgetArea;
+
+    /**
+     * Cached value for our CacheKey. It's not all that cheap to generate it, so, we should only do it once per
+     * request
+     *
+     * @var string|null
+     */
+    private $cacheKey;
+
+    /**
+     * @codeCoverageIgnore
+     * @return string
+     */
+    public function getType(): string
+    {
+        return static::config()->get('default_title');
+    }
+
+    /**
+     * @codeCoverageIgnore
+     * @return FieldList
+     */
+    public function getCMSFields(): FieldList
+    {
+        $fields = parent::getCMSFields();
+
+        // Removing scaffold fields so that they can be added more explicitly (and allowing for update via extension
+        // points)
+        $fields->removeByName([
+            'ShowPagination',
+            'ShowWidgets',
+        ]);
+
+        if (static::config()->get('show_pagination_field')) {
+            $showPaginationField = CheckboxField::create('ShowPagination');
+
+            $this->invokeWithExtensions('updateShowPaginationField', $showPaginationField);
+
+            $fields->addFieldToTab(
+                'Root.Main',
+                $showPaginationField
+            );
+        }
+
+        if (static::config()->get('show_widgets_field')) {
+            $showWidgetsField = CheckboxField::create('ShowWidgets');
+
+            $this->invokeWithExtensions('updateShowWidgetsField', $showWidgetsField);
+
+            $fields->addFieldToTab(
+                'Root.Main',
+                $showWidgetsField
+            );
+        }
+
+        if (static::config()->get('show_info_message_field')) {
+            $messageField = LiteralField::create(
+                'BlockInfoMessage',
+                sprintf(
+                    '<p style="text-align:center">%s</p>',
+                    'This block will automatically display Blog Posts and pagination'
+                )
+            );
+
+            $this->invokeWithExtensions('updateMessageField', $messageField);
+
+            $fields->addFieldToTab(
+                'Root.Main',
+                $messageField
+            );
+        }
+
+        return $fields;
+    }
+
+    public function populateDefaults(): void
+    {
+        parent::populateDefaults();
+
+        if (static::config()->get('set_default_title')) {
+            $this->Title = static::config()->get('default_title');
+        }
+
+        $this->ShowPagination = static::config()->get('pagination_field_default');
+        $this->ShowWidgets = static::config()->get('widgets_field_default');
+    }
+
+    /**
+     * @return DataList
+     * @throws ValidationException
+     */
+    public function getBlogPosts(): ?DataList
+    {
+        if ($this->blogPosts !== null) {
+            return $this->blogPosts;
+        }
+
+        /** @var Blog $page */
+        $page = $this->getPage();
+
+        // Ideally, we want to fetch the BlogPosts that were specifically posted under this Blog page, but, if this
+        // Block is not being used on the Blog page, then that is not possible, and so instead, we'll just return all
+        // Blog Posts in the DB. You can then update either of these lists (maybe with additional filters/limits/etc) by
+        // using the `updateBlogPosts` extension point below
+        if ($page instanceof Blog) {
+            $blogPosts = $page->getBlogPosts();
+        } else {
+            $blogPosts = BlogPost::get();
+        }
+
+        $this->invokeWithExtensions('updateBlogPosts', $blogPosts);
+
+        $this->blogPosts = $blogPosts;
+
+        return $this->blogPosts;
+    }
+
+    /**
+     * @return PaginatedList|null
+     * @throws ValidationException
+     */
+    public function getPaginatedList(): ?PaginatedList
+    {
+        if ($this->paginatedList !== null) {
+            return $this->paginatedList;
+        }
+
+        /** @var BlogController $controller */
+        $controller = Controller::curr();
+
+        // Ideally, we want to fetch the PaginatedList from the BlogController, but, if this Block is not being used on
+        // a Blog page, then that will not be (immediately) possible. You have two options:
+        // 1) You can use the `updatePaginatedList` extension point to apply your filters/limits there
+        // 2) You can implement a method `getBlogPostPaginatedList` on your Controller, and provide it with the
+        //    appropriate data
+        if ($controller instanceof BlogController) {
+            $paginatedList = $controller->PaginatedList();
+        } else {
+            if ($controller->hasMethod('getBlogPostPaginatedList')) {
+                $paginatedList = $controller->getBlogPostPaginatedList();
+            } else {
+                $paginatedList = PaginatedList::create($this->getBlogPosts());
+            }
+        }
+
+        $this->invokeWithExtensions('updatePaginatedList', $paginatedList);
+
+        $this->paginatedList = $paginatedList;
+
+        return $this->paginatedList;
+    }
+
+    /**
+     * @return WidgetArea|null
+     * @throws ValidationException
+     */
+    public function SideBarView(): ?WidgetArea
+    {
+        if ($this->widgetArea !== null) {
+            return $this->widgetArea;
+        }
+
+        $page = $this->getPage();
+
+        // We can't get widgets for the Page if the Page type doesn't have the Widget extension
+        if (!$page->hasExtension(WidgetPageExtension::class)) {
+            return null;
+        }
+
+        // If the Page is inheriting it's SideBar, then we should grab it from the Parent
+        if ($page->InheritSideBar
+            && ($parent = $page->getParent())
+            && $parent->hasMethod('SideBarView')
+        ) {
+            $this->widgetArea = $parent->SideBarView();
+
+            return $this->widgetArea;
+        }
+
+        // Otherwise, let's attempt to fetch the SideBar from this current Page
+        if ($page->SideBar()->exists()) {
+            $this->widgetArea = $page->SideBar();
+
+            return $this->widgetArea;
+        }
+
+        return null;
+    }
+
+    /**
+     * If you're using partial cache, then use can use this as the cache key for the Block
+     *
+     * @return string|null
+     * @throws ValidationException
+     */
+    public function getCacheKey(): ?string
+    {
+        if ($this->cacheKey !== null) {
+            return $this->cacheKey;
+        }
+
+        $cacheKey = implode(
+            '-',
+            [
+                $this->ClassName,
+                $this->ID,
+                $this->LastEdited,
+                $this->getBlogPosts()->count(),
+                $this->getBlogPosts()->max('LastEdited')
+            ]
+        );
+
+        $this->invokeWithExtensions('updateCacheKey', $cacheKey);
+
+        $this->cacheKey = $cacheKey;
+
+        return $this->cacheKey;
+    }
+}
